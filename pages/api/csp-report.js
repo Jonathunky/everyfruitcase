6637778abe8f84ada7b780f02bc2139657776067
Jonathunky <12529409+Jonathunky@users.noreply.github.com>
2023-10-13 23:32:56 +0200

    No unsafe-inline anymore

--- pages/api/csp-report.js
+++ pages/api/csp-report.js
@@ -1,10 +1,14 @@
+export const config = {
+    runtime: "edge",
+}
+
 export default function (req, res) {
-  const report = req.body;
+    const report = req.body
 
-  // You can log, store, or process the CSP report as needed.
-  // For this example, we'll just log it.
-  console.log("CSP Report:", report);
+    // You can log, store, or process the CSP report as needed.
+    // For this example, we'll just log it.
+    console.log("CSP Report:", report)
 
-  // Respond with a 204 No Content status.
-  res.status(204).end();
+    // Respond with a 204 No Content status.
+    res.status(204).end()
 }
